dfe236f1fe59010a3ce011c3a8f6d77eb45555ef
galletafromjell666 <giococu666@gmail.com>
2023-04-12 22:43:10 -0600

    Add ListView for empleados

--- aplicaciones/empleado/urls.py
+++ aplicaciones/empleado/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('empleado/crear-empleado/',
          views.CrearEmpleado.as_view(), name='crear-empleado'),
            path('empleado/crear-empleado2/',
-         views.CrearEmpleado2.as_view(), name='crear-empleado2')
+         views.CrearEmpleado2.as_view(), name='crear-empleado2'),
+           path('empleado/listar-empleados/',
+         views.ListarEmpleados.as_view(), name='listar-empleados'),
 ]
--- aplicaciones/empleado/views.py
+++ aplicaciones/empleado/views.py
@@ -5,13 +5,18 @@ from django.urls import reverse_lazy
 from .forms import EmpleadoForm
 # Create your views here.
 
+class ListarEmpleados(ListView):
+    template_name = 'empleado/listar-empleados.html'
+    model = Empleado
+    context_object_name = 'lista_empleados'
+
 class CrearEmpleado(CreateView):
     template_name = 'empleado/crear-empleado.html'
     model = Empleado
     fields = ['nombre','correo', 'sueldo', 'activo','cargo']
-    success_url = '/inicio/hola/'
+    success_url = reverse_lazy('empleados_app:listar-empleados')
 
 class CrearEmpleado2(CreateView):
     template_name = 'empleado/crear-empleado.html'
     form_class =  EmpleadoForm
-    success_url = '/inicio/hola/'
+    success_url = reverse_lazy('empleados_app:listar-empleados')
